7921933f8624e671e3d09828bd747f90925050a1
Justin Byrne <theskaterjesus@gmail.com>
2021-08-01 22:28:43 +0100

    Design complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        violet: 'hsl(263, 55%, 52%)',
+        dgrayBlue: 'hsl(217, 19%, 35%)',
+        blackBlue: 'hsl(219, 29%, 14%)',
+        lightGray: 'hsl(0, 0%, 81%)',
+        lgrayBlue: 'hsl(210, 46%, 95%)',
+      },
+      fontFamily: {
+        barlow: 'Barlow Semi Condensed, sans-serif',
+      }
+    },
   },
   variants: {
     extend: {},
